18655c378648e4b315c77e1b30ea4b54b2a81c62
vasketz <anfevach@gmail.com>
2021-09-21 00:13:45 +0000

    Update task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """ Page Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """ Return json status code """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """comment"""
+    tbl_obj = {"amenities": storage.count('Amenity'),
+               "cities": storage.count('City'),
+               "places": storage.count('Place'),
+               "reviews": storage.count('Review'),
+               "states": storage.count('State'),
+               "users": storage.count('User')}
+    return jsonify(tbl_obj)
