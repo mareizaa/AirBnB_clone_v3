2ab70a2098b89d9dd2538f4ea3483d610c13a381
vasketz <anfevach@gmail.com>
2021-09-21 01:04:38 +0000

    Update tak 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,8 @@
 """Handle sessions between API"""
 
 
-from flask import Flask
+from flask import Flask, make_response
+from flask.json import jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,8 +15,14 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(error):
-    """ """
+    """ close session between API"""
     storage.close()
+
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    """ Show an error with jsonify """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
